94c5fc808031042f387da2aa5c94fcaba3775cd9
Erel Segal-Halevi <erelsgl@gmail.com>
2024-01-16 13:34:16 +0200

    solutions

--- 02-pareto-efficiency/code/4-cvxpy-demo-product.py
+++ 02-pareto-efficiency/code/4-cvxpy-demo-product.py
@@ -11,12 +11,20 @@ subject to   x+y==1
 
 import cvxpy
 
-x, y = cvxpy.Variable(2)
+# x, y = cvxpy.Variable(2)
+# prob = cvxpy.Problem(objective=cvxpy.Maximize(x*y), constraints=[x+y==10, x>=0, y>=0])
+# # prob = cvxpy.Problem(objective=cvxpy.Maximize(cvxpy.log(x)+cvxpy.log(y)), constraints=[x+y==10, x>=0, y>=0])
+# prob.solve()  # cvxpy.error.DCPError: Problem does not follow DCP rules. Specifically: The objective is not DCP. Its following subexpressions are not: var1[0] @ var1[1]
+# print("status:", prob.status)
+# print("optimal value", prob.value)
+# print("optimal var", x.value, y.value)
 
-prob = cvxpy.Problem(objective=cvxpy.Maximize(x*y), constraints=[x+y==10, x>=0, y>=0])
-# prob = cvxpy.Problem(objective=cvxpy.Maximize(cvxpy.log(x)+cvxpy.log(y)), constraints=[x+y==10, x>=0, y>=0])
+t = 2/3
+x  = cvxpy.Variable(1)
+prob = cvxpy.Problem(objective=cvxpy.Maximize(cvxpy.log(x) + cvxpy.log(1-t*x)), constraints=[x>=0, x<=1])
 prob.solve()  # cvxpy.error.DCPError: Problem does not follow DCP rules. Specifically: The objective is not DCP. Its following subexpressions are not: var1[0] @ var1[1]
 print("status:", prob.status)
 print("optimal value", prob.value)
-print("optimal var", x.value, y.value)
+print("optimal var", x.value)
 
+# x= 1/2t
