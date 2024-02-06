861ec5c7ae29c4bc53e362a34eb30e383562dc11
Erel Segal-Halevi <erelsgl@gmail.com>
2024-02-06 12:35:29 +0200

    example

--- 06-item-allocation-with-money/code/max-value-assignment.py
+++ 06-item-allocation-with-money/code/max-value-assignment.py
@@ -17,17 +17,17 @@ print("\n\nThere are three tennants and three rooms.")
 G=nx.Graph()
 
 # Add edges with weights:
-G.add_edge('aya','martef' ,weight=2)
+G.add_edge('aya','martef' ,weight=20)
 G.add_edge('aya','heder',weight=40)
 G.add_edge('aya','salon' ,weight=35)
 
 G.add_edge('batya','martef' ,weight=40)
-G.add_edge('batya','heder',weight=3)
+G.add_edge('batya','heder',weight=30)
 G.add_edge('batya','salon' ,weight=35)
 
 G.add_edge('gila','martef' ,weight=20)
 G.add_edge('gila','heder',weight=40)
-G.add_edge('gila','salon' ,weight=4)
+G.add_edge('gila','salon' ,weight=40)
 
 print("Maximum-value matching: ", nx.max_weight_matching(G))
 
